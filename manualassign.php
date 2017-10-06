6099670979cb1539a2c2ae65ed544d62726dd76e
Ilya Stoliar <i.difki@gmail.com>
2017-10-06 12:41:20 +0300

    Update manualassign.php

--- manualassign.php
+++ manualassign.php
@@ -5,7 +5,7 @@
 
   <head>
     <meta charset="UTF-8">
-    <title>שיבוץ ידני</title>
+    <title>שיבוץ ידני test</title>
     <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,300,600' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="css/normalize.css">
         <link rel="stylesheet" href="css/style.css">
