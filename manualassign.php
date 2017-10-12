42bb1cecc8165e42fe73c9f3337d711f38e422f7
Ilya Stoliar <i.difki@gmail.com>
2017-10-12 11:05:29 +0300

    Update manualassign.php

--- manualassign.php
+++ manualassign.php
@@ -210,14 +210,14 @@ echo'<center><h1><u> שיבוץ ידני</u></h1>';
 								
 								$num_asked=mysqli_fetch_array($num_shifts_asked);
 								$num=mysqli_fetch_array($num_of_shift);
-								if ($num[0]==$num_asked[0])
-									$color="green";
+								//if ($num[0]==$num_asked[0])
+								//	$color="green";
 								//elseif ($num[0]>$num_asked[0])
 								//	$color="red";
 								//elseif ($num[0]<$num_asked[0])
 								//	$color="yellow";
 									
-								echo "<tr bgcolor="$color"><td>$num[0]</td><td>$num_asked[0]</td><td>$id[1]</td></tr>";
+								echo "<tr bgcolor="green"><td>$num[0]</td><td>$num_asked[0]</td><td>$id[1]</td></tr>";
 							}
 							  echo "</table><br><br>";
            
