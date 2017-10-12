dd66af2d95bd7a05cedfc92bf16c8354b047f54f
Ilya Stoliar <i.difki@gmail.com>
2017-10-12 11:07:14 +0300

    Update manualassign.php

--- manualassign.php
+++ manualassign.php
@@ -217,7 +217,8 @@ echo'<center><h1><u> שיבוץ ידני</u></h1>';
 								//elseif ($num[0]<$num_asked[0])
 								//	$color="yellow";
 									
-								echo "<tr bgcolor="green"><td>$num[0]</td><td>$num_asked[0]</td><td>$id[1]</td></tr>";
+								//echo "<tr bgcolor="green"><td>$num[0]</td><td>$num_asked[0]</td><td>$id[1]</td></tr>";
+								echo "hello"
 							}
 							  echo "</table><br><br>";
            
